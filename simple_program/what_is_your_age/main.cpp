07690abb0e7c0e97f7c530490fdb733bf428bac7
Yuvraj Chauhan <yuvchauhan15@gmail.com>
2024-08-19 22:47:03 -0400

    What is your name cpp program

---
+++ simple_program/what_is_your_age/main.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "What is your age?" << endl;
+    cin >> age;
+    cout << "Your age is:\n";
+    cout << age;
+    return 0;
+}
